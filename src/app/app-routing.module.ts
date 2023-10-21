f6d41de511eb0fb38c1e4aeda5d362fd06dc9bd9
Lucas Ruiz <85413494+U202110085@users.noreply.github.com>
2023-10-20 21:55:39 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { DocumentationViewComponent } from './views/documentation-view/documentation-view.component';
+import { TeamViewComponent } from './views/team-view/team-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeViewComponent },
+  { path: 'team', component: TeamViewComponent },
+  { path: 'documentation', component: DocumentationViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/shared/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { TeamViewComponent } from './views/team-view/team-view.component';
+import { DocumentationViewComponent } from './views/documentation-view/documentation-view.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { FormComponent } from './components/home/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    TeamViewComponent,
+    DocumentationViewComponent,
+    HeaderComponent,
+    FooterComponent,
+    FormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
