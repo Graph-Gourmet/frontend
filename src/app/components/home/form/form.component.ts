1bf4ffb2667174ea86fa10c1d2a32f5ab94823b0
lucas1336 <lucasruiz1336@gmail.com>
2023-11-05 18:26:31 -0500

    feat: added list next to form

--- src/app/components/home/form/form.component.ts
+++ src/app/components/home/form/form.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, ElementRef, ViewChild, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { MatChipInputEvent } from '@angular/material/chips';
 import { Observable, map, startWith } from 'rxjs';
 import { RecipeRequest } from 'src/app/models/recipe-request.model';
+import { ContentObserver } from '@angular/cdk/observers';
 
 @Component({
   selector: 'app-form',
@@ -9,9 +14,6 @@ import { RecipeRequest } from 'src/app/models/recipe-request.model';
   styleUrls: ['./form.component.css'],
 })
 export class FormComponent {
-  // recipeForm: FormGroup;
-  // recipeRequest = new RecipeRequest();
-
   recipes = [
     'Spaghetti Bolognese',
     'Chicken Alfredo',
@@ -35,22 +37,17 @@ export class FormComponent {
     'Chocolate Chip Cookies',
   ];
 
-  // constructor(private fb: FormBuilder) {
-  //   this.recipeForm = this.fb.group({});
-  // }
-
   myControl = new FormControl('');
-  options: string[] = ['One', 'Two', 'Three'];
   filteredOptions!: Observable<string[]>;
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map((value) => this._filter(value || ''))
+      map((value) => this._filterRecipes(value || ''))
     );
   }
 
-  private _filter(value: string): string[] {
+  private _filterRecipes(value: string): string[] {
     const filterValue = value.toLowerCase();
 
     const filteredRecipes = this.recipes.filter((option) =>
@@ -58,5 +55,66 @@ export class FormComponent {
     );
 
     return filteredRecipes.slice(0, 10);
+  }
+
+  ///////////////////////////////////
+
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  ingredientCtrl = new FormControl('');
+  filteredIngredients: Observable<string[]>;
+  ingredients: string[] = ['Lemon'];
+  allIngrediens: string[] = ['Apple', 'Lemon', 'Lime', 'Orange', 'Strawberry'];
+
+  @ViewChild('ingredientInput') ingredientInput!: ElementRef<HTMLInputElement>;
+
+  announcer = inject(LiveAnnouncer);
+
+  constructor() {
+    this.filteredIngredients = this.ingredientCtrl.valueChanges.pipe(
+      startWith(null),
+      map((ingredient: string | null) =>
+        ingredient
+          ? this._filterIngredients(ingredient)
+          : this.allIngrediens.slice()
+      )
+    );
+  }
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (
+      !this.ingredients.includes(value) &&
+      this.allIngrediens.includes(value)
+    ) {
+      this.ingredients.push(value);
+    }
+    event.chipInput!.clear();
+    this.ingredientCtrl.setValue(null);
+  }
+
+  remove(ingredient: string): void {
+    const index = this.ingredients.indexOf(ingredient);
+
+    if (index >= 0) {
+      this.ingredients.splice(index, 1);
+
+      this.announcer.announce(`Removed ${ingredient}`);
+    }
+  }
+
+  selected(event: MatAutocompleteSelectedEvent): void {
+    if (!this.ingredients.includes(event.option.viewValue)) {
+      this.ingredients.push(event.option.viewValue);
+    }
+    this.ingredientInput.nativeElement.value = '';
+    this.ingredientCtrl.setValue(null);
+  }
+
+  private _filterIngredients(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    const filterIngredients = this.ingredients.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+    return filterIngredients.slice(0, 10);
   }
 }
