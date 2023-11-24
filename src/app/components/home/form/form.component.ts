918ecc837a1879dc631bed3efdae96f3a4eb569f
lucas1336 <lucasruiz1336@gmail.com>
2023-11-24 13:46:09 -0500

    fix: impede value < 0

--- src/app/components/home/form/form.component.ts
+++ src/app/components/home/form/form.component.ts
@@ -77,80 +77,95 @@ export class FormComponent {
     this.minPriceControl.valueChanges.subscribe((minPrice) => {
       if (
         minPrice > this.maxPriceControl.value &&
-        this.maxPriceControl.value != null
+        (this.maxPriceControl.value != null || this.maxPriceControl.value == 0)
       ) {
         this.maxPriceControl.setValue(minPrice);
       }
+      if (minPrice < 0) this.minPriceControl.setValue(0);
     });
     this.maxPriceControl.valueChanges.subscribe((maxPrice) => {
       if (maxPrice < this.minPriceControl.value) {
         this.minPriceControl.setValue(maxPrice);
       }
+      if (maxPrice < 0) this.maxPriceControl.setValue(0);
     });
     this.minCaloriesControl.valueChanges.subscribe((minCalories) => {
       if (
         minCalories > this.maxCaloriesControl.value &&
-        this.maxCaloriesControl.value != null
+        (this.maxCaloriesControl.value != null ||
+          this.maxCaloriesControl.value == 0)
       ) {
         this.maxCaloriesControl.setValue(minCalories);
       }
+      if (minCalories < 0) this.minCaloriesControl.setValue(0);
     });
     this.maxCaloriesControl.valueChanges.subscribe((maxCalories) => {
       if (maxCalories < this.minCaloriesControl.value) {
         this.minCaloriesControl.setValue(maxCalories);
       }
+      if (maxCalories < 0) this.maxCaloriesControl.setValue(0);
     });
     this.minCarbohydratesControl.valueChanges.subscribe((minCarbohydrates) => {
       if (
         minCarbohydrates > this.maxCarbohydratesControl.value &&
-        this.maxCarbohydratesControl.value != null
+        (this.maxCarbohydratesControl.value != null ||
+          this.maxCarbohydratesControl.value == 0)
       ) {
         this.maxCarbohydratesControl.setValue(minCarbohydrates);
       }
+      if (minCarbohydrates < 0) this.minCarbohydratesControl.setValue(0);
     });
     this.maxCarbohydratesControl.valueChanges.subscribe((maxCarbohydrates) => {
       if (maxCarbohydrates < this.minCarbohydratesControl.value) {
         this.minCarbohydratesControl.setValue(maxCarbohydrates);
       }
+      if (maxCarbohydrates < 0) this.maxCarbohydratesControl.setValue(0);
     });
     this.minProteinControl.valueChanges.subscribe((minProtein) => {
       if (
         minProtein > this.maxProteinControl.value &&
-        this.maxProteinControl.value != null
+        (this.maxProteinControl.value != null ||
+          this.maxProteinControl.value == 0)
       ) {
         this.maxProteinControl.setValue(minProtein);
       }
+      if (minProtein < 0) this.minProteinControl.setValue(0);
     });
     this.maxProteinControl.valueChanges.subscribe((maxProtein) => {
       if (maxProtein < this.minProteinControl.value) {
         this.minProteinControl.setValue(maxProtein);
       }
+      if (maxProtein < 0) this.maxProteinControl.setValue(0);
     });
     this.minFatControl.valueChanges.subscribe((minFat) => {
       if (
         minFat > this.maxFatControl.value &&
-        this.maxFatControl.value != null
+        (this.maxFatControl.value != null || this.maxFatControl.value == 0)
       ) {
         this.maxFatControl.setValue(minFat);
       }
+      if (minFat < 0) this.minFatControl.setValue(0);
     });
     this.maxFatControl.valueChanges.subscribe((maxFat) => {
       if (maxFat < this.minFatControl.value) {
         this.minFatControl.setValue(maxFat);
       }
+      if (maxFat < 0) this.maxFatControl.setValue(0);
     });
     this.minSugarControl.valueChanges.subscribe((minSugar) => {
       if (
         minSugar > this.maxSugarControl.value &&
-        this.maxSugarControl.value != null
+        (this.maxSugarControl.value != null || this.maxSugarControl.value == 0)
       ) {
         this.maxSugarControl.setValue(minSugar);
       }
+      if (minSugar < 0) this.minSugarControl.setValue(0);
     });
     this.maxSugarControl.valueChanges.subscribe((maxSugar) => {
       if (maxSugar < this.minSugarControl.value) {
         this.minSugarControl.setValue(maxSugar);
       }
+      if (maxSugar < 0) this.maxSugarControl.setValue(0);
     });
   }
 
